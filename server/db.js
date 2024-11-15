a99973e4999ff502c44ed9c5d9b12bd11d696862
RussTP <rpawlett@live.com>
2024-11-15 11:57:52 -0500

    modified db.js file for mongo server config

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:Gl0a1dXJsdOx9XbVpw6TTB87@172.21.167.76:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
